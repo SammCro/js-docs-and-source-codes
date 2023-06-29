4f3da6fe3a0c448ac8657c82156953c293fc85f8
Ilkay Samet Ozturk <ilk.samett06@gmail.com>
2023-06-29 15:33:45 +0300

    prettier config is added.

--- basics.js
+++ basics.js
@@ -1,20 +1,20 @@
 //* let const and var
 
-// let 
-let age = 32 ;
-age = 23 ;
+// let
+let age = 32;
+age = 23;
 
 // const
-const constAge = 32 ;
-age = 23 ; // error
+const constAge = 32;
+age = 23; // error
 // also can not create empty variable with const declarition
 // always try to use const
 
 // var
-var varAge = 32 ;
-varAge = 23 ;
+var varAge = 32;
+varAge = 23;
 
-//* string literals 
+//* string literals
 
 const ilkay = "I'am " + "ilkay " + "and " + 23 + " years old.";
 const myName = "İlkay";
@@ -22,48 +22,47 @@ console.log(`I am ${myName} \n`);
 
 //* type conversion
 
-
-let score = '100';
+let score = "100";
 console.log(score + 1); // 1001
 score = Number(score);
 console.log(score + 1); // 101
 
-console.log(Number("samet")) // NaN number
-console.log('23' + '10' + 3) // 23103
-console.log('23' + '10' - 3) // 2307
+console.log(Number("samet")); // NaN number
+console.log("23" + "10" + 3); // 23103
+console.log("23" + "10" - 3); // 2307
 
-//* falsy values 
+//* falsy values
 
 // 0 '' undefined null NaN
 
-//* equality operators loose strict 
+//* equality operators loose strict
 
 const age1 = 25;
-if (age1 === 25) console.log('You are 25 years old (strict)');
+if (age1 === 25) console.log("You are 25 years old (strict)");
 
-if (age1 == 25) console.log('You are 25 years old (loose)');
+if (age1 == 25) console.log("You are 25 years old (loose)");
 
-const age2 = '25';
-if (age2 === 25) console.log('You are 25 years old (strict)');
+const age2 = "25";
+if (age2 === 25) console.log("You are 25 years old (strict)");
 
-if (age2 == 25) console.log('You are 25 years old (loose)');
+if (age2 == 25) console.log("You are 25 years old (loose)");
 // loose equality operator converts the type of the value
 // strict equality operator does not convert the type of the value
 
 //* switch statement
 
 const number = 3;
-switch(number) {
-    case 1:
-    case 3:
-        console.log('number is 1');
-        break;
-    case 2: 
-        console.log('number is 2');
-        break;
-    default:
-        console.log('number is not 1 or 2');
-        break;
+switch (number) {
+  case 1:
+  case 3:
+    console.log("number is 1");
+    break;
+  case 2:
+    console.log("number is 2");
+    break;
+  default:
+    console.log("number is not 1 or 2");
+    break;
 }
 
 //* conditional operator
--- basics2.js
+++ basics2.js
@@ -1,15 +1,17 @@
 'use strict';
- 
+
+const { func } = require('prop-types');
+
 // strict mode prevents declare variable without let or const keyword.
 // also disable reserverd variable name declaration.
 
-//* functions 
+//* functions
 
 // function declaration
 
-function fruitProcessor(apples, oranges){ 
-    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
-    return juice;
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
 }
 
 const appleJuice = fruitProcessor(5, 0);
@@ -17,14 +19,108 @@ console.log(appleJuice);
 
 // function expression
 
-const fruitProcessorNew = function(apples, oranges) {
-    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
-    return juice;
-}
+const fruitProcessorNew = function (apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+};
 
 const appleJuiceNew = fruitProcessorNew(5, 0);
 console.log(appleJuiceNew);
 
-
 // arrow function
 
+const fruitProcessorArrow = (apples, oranges) =>
+  `Juice ${apples} and ${oranges}`;
+console.log(fruitProcessorArrow(1, 2));
+
+//* arrays
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const years = new Array(1, 2, 3);
+console.log(years);
+
+// only primative types can not be mutable
+// arrays are mutable
+
+friends[2] = 'Jay'; // okey
+// friends = ['Bob', 'Alice']; // not okey
+
+// in javascript arrays can hold different types of variables
+const jonas = ['Jonas', 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+
+//* array methods
+
+// push and pop
+// push adds element to the end of the array
+// pop removes last element of the array
+
+const friendsNew = ['Samet', 'İlkay'];
+friendsNew.push('Mehmet');
+
+console.log(friendsNew);
+friendsNew.pop();
+console.log(friendsNew);
+
+// unshift and shift
+// unshift adds element to the beginning of the array
+// shift removes first element of the array
+let numbers = [468, 54, 23, 12];
+numbers.unshift(1);
+console.log(numbers);
+numbers.shift();
+console.log(numbers);
+
+// indexOf
+console.log(numbers.indexOf(12));
+
+// includes
+const doesInclude = numbers.includes(12);
+console.log(doesInclude);
+
+//* objects
+
+const ilkayObject = {
+  firstName: 'İlkay',
+  age: 23,
+  job: 'Software Engineer',
+  friends: ['Samet', 'Mehmet', 'Ahmet'],
+  buffer: function () {
+    console.log(`${this.age}`);
+  },
+};
+
+console.log(ilkayObject.firstName);
+console.log(ilkayObject['first' + 'Name']);
+
+ilkayObject.friends.push('Fiko');
+
+console.log(ilkayObject);
+ilkayObject.buffer();
+
+//* loops
+
+// for loop
+for (let i = 0; i < 100; i++) {
+  //console.log(i);
+}
+
+for (let i = 31; i > 0; i--) {
+  //console.log(i);
+}
+
+// while loop
+
+let i = 0;
+while (i < 10) {
+  i++;
+}
+
+// dice loop with while loop
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+while (dice !== 5 && dice != 4) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+}
